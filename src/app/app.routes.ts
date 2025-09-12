b99e374b3fcb8b8135f5335dbf970d10412cd478
Cocoyanomami <cocoyanomami@gmail.com>
2025-09-11 21:52:23 -0600

    home ffinish

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { ContactoComponent } from './pages/contacto/contacto.component';
 import { CertificationsComponent } from './pages/certifications/certifications.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
+import { AboutComponent } from './pages/about/about.component';
 
 export const routes: Routes = [
 
@@ -10,5 +11,6 @@ export const routes: Routes = [
     {path: 'contacto' , component: ContactoComponent},
     {path: 'projects' , component: ProjectsComponent},
     {path: 'certifiaciones', component: CertificationsComponent},
+    {path: 'About', component: AboutComponent},
     {path: '**', redirectTo: ''} 
 ];
